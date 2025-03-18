fc9f8549b6ae01c5cc188337324375bb53b779d0
Rafaelakamexican <739415@student.dusd.net>
2025-03-18 14:38:59 -0700

    GetComponet
    Added 3 scripts

---
+++ ScrpitingProjectP8Horn/Assets/Scripts/YetAnotherScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YetAnotherScript : MonoBehaviour
+{
+    public int numberOfPlayersDeaths = 3;
+    internal string numberOfPlayerDeaths;
+}
